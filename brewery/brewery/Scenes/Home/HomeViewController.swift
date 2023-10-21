0354fa934591d1b1ab3b64d6424434834792f49f
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-10-21 00:17:07 -0300

    update na homeView

--- brewery/brewery/Scenes/Home/HomeViewController.swift
+++ brewery/brewery/Scenes/Home/HomeViewController.swift
@@ -7,10 +7,88 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+private enum Section: Hashable {
+    case main
+}
+
+private enum Item: Hashable {
+    case beer(Beer)
+}
+
+final class HomeViewController: UIViewController {
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>?
+    
+    private lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .none
+        view.register(BeerCollectionViewCell.self, forCellWithReuseIdentifier: BeerCollectionViewCell.cellIdentifier())
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Home"
         view.backgroundColor = .purple
+        setupDataSource()
+        setupLayout()
+        setupConstraint()
+        
+        var recentsSnapshot = NSDiffableDataSourceSectionSnapshot<Item>()
+        recentsSnapshot.append([Item.beer(Beer(id: 1, name: "teste", tagline: "teste", description: "testew", imageURL: "teste"))])
+        
+        self.dataSource?.apply(recentsSnapshot, to: .main, animatingDifferences: false)
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let sectionLayout: NSCollectionLayoutSection
+            
+            let itemSize: NSCollectionLayoutSize = .init(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(101))
+            let item: NSCollectionLayoutItem = .init(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+            
+            let groupSize: NSCollectionLayoutSize = .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let group: NSCollectionLayoutGroup = .vertical(layoutSize: groupSize, subitems: [item])
+            
+            sectionLayout = NSCollectionLayoutSection(group: group)
+            
+            return sectionLayout
+        }
+        
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
+    }
+    
+    private func setupDataSource() {
+        self.dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: Item) -> UICollectionViewCell? in
+            
+            switch item {
+                
+            case .beer:
+                
+                let cell: BeerCollectionViewCell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: BeerCollectionViewCell.cellIdentifier(),
+                    for: indexPath
+                ) as! BeerCollectionViewCell
+                
+                return cell
+            }
+        }
+    }
+    
+    private func setupLayout() {
+        view.addSubview(collectionView)
+        collectionView.collectionViewLayout = createLayout()
+    }
+    
+    private func setupConstraint() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
 }
