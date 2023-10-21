d9c22605eb41c75ac770d39c187059d6e1b2a298
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-10-20 23:43:50 -0300

    criacao da model

---
+++ brewery/brewery/Models/Beer.swift
@@ -0,0 +1,20 @@
+//
+//  Beer.swift
+//  brewery
+//
+//  Created by Filipe Boeck on 20/10/23.
+//
+
+import Foundation
+
+struct Beer: Codable, Identifiable {
+    let id: Int
+    let name, tagline, description: String
+    let imageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, tagline
+        case description
+        case imageURL = "image_url"
+    }
+}
